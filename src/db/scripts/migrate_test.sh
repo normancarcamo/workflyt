6cb5be5639f987b62aa9bf6315b9219ed4776c33
Norman Carcamo <normancarcamo@gmail.com>
2019-01-21 05:31:12 -0600

    scripts de prueba para migrar la base de datos

--- src/db/scripts/migrate_test.sh
+++ src/db/scripts/migrate_test.sh
@@ -1,3 +1,3 @@
-psql -h 127.0.0.1 -U dev -d disema_test -f ./src/db/scripts/start.sql
+psql -U dev -d disema_test -h 127.0.0.1 -f ./src/db/scripts/start.sql
 NODE_ENV=test
 node_modules/.bin/sequelize db:migrate
